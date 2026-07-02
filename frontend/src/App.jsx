2b465c72ab0ecb4291c9d05fe4a33f259c23c5c9
Yan <nubelaprofesora2021@gmail.com>
2026-07-02 11:14:32 +0200

    feat:import and add Navbar to App layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import GroupPage from "./pages/GroupPage";
 import JoinPage from "./pages/JoinPage";
- 
+import Navbar from "./components/Navbar"; 
+
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/groups/:groupId" element={<GroupPage />} />
-        <Route path="/join/:inviteCode" element={<JoinPage />} />
-      </Routes>
+      <Navbar />
+      <main className="app-container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/groups/:groupId" element={<GroupPage />} />
+          <Route path="/join/:inviteCode" element={<JoinPage />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }
