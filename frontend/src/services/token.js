3db3f93d0744155cf6d3f8b5c7630eb50eaf3270
Anastasiia Hlushkova <glushkova.a.y@gmail.com>
2026-07-04 20:08:19 +0200

    updated token.js file

--- frontend/src/services/token.js
+++ frontend/src/services/token.js
@@ -1,10 +1,19 @@
-let token = null
+const TOKEN_KEY = 'splitit_token'
 
-export function setToken(t) { token = t }
-export function getToken() { return token }
-export function clearToken() { token = null }
+export function setToken(t) {
+  localStorage.setItem(TOKEN_KEY, t)
+}
+
+export function getToken() {
+  return localStorage.getItem(TOKEN_KEY)
+}
+
+export function clearToken() {
+  localStorage.removeItem(TOKEN_KEY)
+}
 
 export function authHeaders() {
+  const token = getToken()
   return token
     ? { 'Content-Type': 'application/json', Authorization: `Bearer ${token}` }
     : { 'Content-Type': 'application/json' }
